903a0c85d1b8bc4c077cf2c4c9ddf59a6116029f
i-douk <101892232+i-douk@users.noreply.github.com>
2025-08-23 11:44:21 +0100

    1.11

--- ping_pong/main.ts
+++ ping_pong/main.ts
@@ -1,7 +1,10 @@
-
+const logDir = "/usr/src/app/files";
+const filePath = `${logDir}/logs.txt`;
 let pingpong = 0;
 
 Deno.serve( { port : 4243} , (_req) => {
   pingpong++
+  const pingEntry = `Ping / Pong : ${pingpong}\n`;
+  Deno.writeTextFileSync(filePath, pingEntry, { append: true });
   return new Response( "Ping:" + String(pingpong) )
 })
